04b9cf7a2387eb32337a53076e4b9140dd049145
Seungjae <s34yoo@uwaterloo.ca>
2024-01-18 10:21:10 -0500

    supabase + calendar api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function App() {
+  const session = useSession(); // tokens
+  const supabase = useSupabaseClient(); // talk to  supabase
+  
+  async function createCalenderEvent() {
+    const event = {
+      'summary': 'Google I/O 2015',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2024-01-28T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles'
+      },
+      'end': {
+        'dateTime': '2024-01-28T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles'
+      }
+    };
+  
+    await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+      method: "POST",
+      headers: {
+        'Authorization': 'Bearer ' + session.provider_token // access token to google
+      },
+      body: JSON.stringify(event)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+    })
+  }
+
+  async function googleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        scopes: 'https://www.googleapis.com/auth/calendar'
+      }
+    });
+    if (error) {
+      alert("Error Logging in to Google Provider with Supabase");
+      console.log(error);
+    }
+  }
+
+  async function signOut() {
+    await supabase.auth.signOut();
+  }
+
+  console.log(session);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {session ? 
+        <>
+          <h2>Hey there {session.user.email}</h2>
+          <button onClick= {() => createCalenderEvent()}>Create Event</button>
+          <button onClick={() => signOut()}> Sign Out</button>
+        </>
+        :
+        <>
+          <button onClick={() => googleSignIn()}> Sign In With Google</button>
+        </>
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+
+const supabase = createClient(
+  "https://jmmnkklrqsukbclocvga.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptbW5ra2xycXN1a2JjbG9jdmdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU1NTg1MTksImV4cCI6MjAyMTEzNDUxOX0.ZXPLH-J84DuF_GvnidDjdnkeuI9IaxAuT1T22psIwfs"
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <SessionContextProvider supabaseClient={supabase}>
+          <App />
+        </SessionContextProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
